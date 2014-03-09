8329b5237cd81c536495f916bfb86ba3e1c1c0cb
Per Flitig <per.flitig@gmail.com>
2014-03-09 01:27:56 +0100

    Removed assertions comment

--- test/yatzy_test.js
+++ test/yatzy_test.js
@@ -2,26 +2,6 @@
 
 var yatzy = require('../lib/yatzy.js');
 
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
-
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
-
 var testHelper = function (test, roll, category, score) {
   test.expect(1);
   test.equal(yatzy.score(roll, category), score, 'should be ' + score);
